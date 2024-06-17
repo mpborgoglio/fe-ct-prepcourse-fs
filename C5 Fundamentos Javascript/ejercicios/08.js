bd583753e31913597cdf99e7e09e4e7f88e02ace
Pia Borgoglio <llive6705@gmail.com>
2024-06-17 14:00:35 -0300

    test 11 clase 5

--- C5 Fundamentos Javascript/ejercicios/08.js
+++ C5 Fundamentos Javascript/ejercicios/08.js
@@ -1,8 +1,16 @@
 function esNumeroEntero(numero) {
-  // La función recibe un argumento "numero".
+   
+    return Number.isInteger(numero);
+  }
+  
+  // Ejemplos de uso:
+  console.log(esNumeroEntero(5)); // Devuelve true
+  console.log(esNumeroEntero(3.14)); // Devuelve false
+  console.log(esNumeroEntero(-10)); // Devuelve true
+   // La función recibe un argumento "numero".
   // Verifica si este es un número entero o no.
   // Returna true si lo es, de lo contrario, retorna false.
   // Tu código:
-}
+
 
 module.exports = esNumeroEntero;
